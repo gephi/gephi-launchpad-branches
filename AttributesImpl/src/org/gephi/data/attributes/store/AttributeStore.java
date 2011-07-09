49b1f4e708e481fc112e762721ece9f24d9e2aab
Ernesto Aneiros <eaneiros@gmail.com>
2011-07-09 15:44:20 -0400

    Apply patch by eduramiba to delete db files on shutdown

--- AttributesImpl/src/org/gephi/data/attributes/store/AttributeStore.java
+++ AttributesImpl/src/org/gephi/data/attributes/store/AttributeStore.java
@@ -17,7 +17,7 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 package org.gephi.data.attributes.store;
 
 import com.sleepycat.bind.ByteArrayBinding;
@@ -25,6 +25,7 @@ import com.sleepycat.bind.tuple.IntegerBinding;
 import com.sleepycat.collections.StoredMap;
 import com.sleepycat.je.Database;
 import com.sleepycat.je.Environment;
+import java.io.File;
 import java.util.Map;
 import net.sf.ehcache.CacheException;
 import net.sf.ehcache.CacheManager;
@@ -40,20 +41,18 @@ import org.gephi.data.attributes.store.serializers.AttributeValueSerializer;
  * @author Ernesto A
  */
 public class AttributeStore implements Store<Integer, Object> {
-    private final String name;
 
+    private final String name;
     private final Environment env;
     private final Database db;
     private final StoredMap<Integer, byte[]> diskStore;
-
     private final CacheManager cacheManager;
     private final Ehcache cache;
-    
     private final AttributeValueSerializer serializer;
-    
+
     public AttributeStore(String name, Environment env, Database db, CacheManager manager) {
         this.name = name;
-        
+
         this.env = env;
         this.db = db;
         IntegerBinding keyBinding = new IntegerBinding();
@@ -64,7 +63,7 @@ public class AttributeStore implements Store<Integer, Object> {
         manager.addCache(name);
         cache = new SelfPopulatingCache(manager.getEhcache(name), getCacheEntryFactory());
         cache.getCacheEventNotificationService().registerListener(getCacheEventListener());
-     
+
         serializer = new AttributeValueSerializer();
     }
 
@@ -111,16 +110,32 @@ public class AttributeStore implements Store<Integer, Object> {
 
     public void close() {
         db.close();
+        File envHome = env.getHome();
         env.close();
+        deleteDirectory(envHome);
         cacheManager.removeCache(name);
+    }
+
+    private static boolean deleteDirectory(File path) {
+        if (path.exists()) {
+            File[] files = path.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isDirectory()) {
+                    deleteDirectory(files[i]);
+                } else {
+                    files[i].delete();
+                }
+            }
+        }
+        return (path.delete());
     }
 
     private Element elementFor(Integer key, Object value) {
         return new Element(key, value);
     }
-    
+
     private CacheEventListener getCacheEventListener() {
-        return  new CacheEventListener() {
+        return new CacheEventListener() {
 
             @Override
             public void notifyElementRemoved(Ehcache cache, Element element) throws CacheException {
@@ -140,9 +155,9 @@ public class AttributeStore implements Store<Integer, Object> {
 
             @Override
             public void notifyElementEvicted(Ehcache cache, Element element) {
-                Integer key = (Integer)element.getObjectKey();
+                Integer key = (Integer) element.getObjectKey();
                 Object val = element.getObjectValue();
-                
+
                 byte[] arr = serializer.writeValueData(val);
                 diskStore.put(key, arr);
                 
@@ -158,12 +173,12 @@ public class AttributeStore implements Store<Integer, Object> {
             }
 
             @Override
-            public  Object clone() throws CloneNotSupportedException {
+            public Object clone() throws CloneNotSupportedException {
                 throw new CloneNotSupportedException();
             }
         };
     }
-    
+
     private CacheEntryFactory getCacheEntryFactory() {
         return new CacheEntryFactory() {
 
@@ -171,17 +186,16 @@ public class AttributeStore implements Store<Integer, Object> {
             public Object createEntry(Object key) throws Exception {
                 try {
                     cache.acquireWriteLockOnKey(key);
-                    
-                    byte[] arr = diskStore.get((Integer)key);
-                    
+
+                    byte[] arr = diskStore.get((Integer) key);
+
                     if (arr != null) {
                         Object val = serializer.readValueData(arr);
                         return val;
                     }
-                    
+
                     return null;
-                }
-                finally {
+                } finally {
                     cache.releaseWriteLockOnKey(key);
                 }
             }
