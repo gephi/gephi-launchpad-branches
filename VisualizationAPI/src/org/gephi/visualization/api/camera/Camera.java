0e49098819318a7cba1291bef5f9eca9da913a5f
Vojtech Bardiovsky <vojtech.bardiovsky@gmail.com>
2011-08-09 20:21:21 +0200

    Copied and completed the documentation into the Camera interface.

--- VisualizationAPI/src/org/gephi/visualization/api/camera/Camera.java
+++ VisualizationAPI/src/org/gephi/visualization/api/camera/Camera.java
@@ -32,80 +32,140 @@ import org.gephi.visualization.api.geometry.AABB;
  * @author Antonio Patriarca <antoniopatriarca@gmail.com>
  */
 public interface Camera {
-
+    
+    /**
+     * Creates a new copy of this camera.
+     * 
+     * @return the copy
+     */
     public Camera copy();
-    
-    
+
+    /**
+     * Sets the screen size. It updates the scale factor to show the same
+     * vertical region of the graph.
+     * 
+     * @param size the screen size
+     */
     public void screenSize(Dimension size);
     
+    /**
+     * Returns the screen width.
+     * 
+     * @return the screen width
+     */
     public float screenWidth();
 
+    /**
+     * Returns the screen height.
+     * 
+     * @return the screen height
+     */
     public float screenHeight();
 
-
+    /**
+     * Moves the center of the screen to <code>center</code>. It ignores the
+     * 3<sup>rd</sup> components of the <code>center</code> vector and the
+     * <code>up</code> completely.
+     * 
+     * @param center the new center of the screen. The third component is not
+     *               used
+     * @param up it is ignored by this method
+     */
     public void lookAt(Vec3Base center, Vec3Base up);
 
     /**
      * Returns the given point as it will appear on the screen together with its
      * size on screen after transformation have been applied.
-     * @return array of floats, where
-     * [0,1] -> point coordinates on screen
+     * 
+     * @param x first component of the point in world coordinates
+     * @param y second component of the point in world coordinates
+     * @param z ignored by this method
+     * @param size the size of the object on screen
+     * @return array of floats, where <br />
+     * [0,1] -> point coordinates on screen <br />
      * [2]   -> size of the node on screen
      */
     public float[] projectPoint(float x, float y, float z, float size);
 
     /**
      * Returns a point from camera viewing plane corresponding to the 2D point
-     * on screen. // TODO this method will be changed to accept parameters
-     * specifying how the point will be projected (e.g intersection with z=0, or
-     * some distance from the camera)
+     * on screen.
      */
     public Vec3 projectPointInverse(float x, float y);
 
     /**
-     * Returns the translation vector which corresponds to the (x,y) screen
-     * vector.
+     * Returns the 3D vector which corresponds to the translation on the screen
+     * by <code>(x,y)</code>. In this case the 
+     * 
+     * @param x the first component of the vector in screen coordinates
+     * @param y the second component of the vector in screen coordinates
+     * @return the translation vector in world coordinate
      */
     public Vec3 projectVectorInverse(float x, float y);
 
     /**
-     * Returns the distance of a point [a,b] on the screen to the projection of
-     * point [x,y,z].
+     * Gets the distance on the screen between a point in world coordinates
+     * and a point in screen coordinates.
+     * 
+     * @param x the first component of the point in world coordinates
+     * @param y the second component of the point in world coordinates
+     * @param z ignored by this method
+     * @param a the first component of the point in screen coordinates
+     * @param b the second component of the point in screen coordinates
+     * @return the distance between the two points on the screen
      */
     public float getPlanarDistance(float x, float y, float z, int a, int b);
 
     /**
+     * Moves the camera by the screen vector <code>(dx, dy)</code>
      * 
-     * @param dx
-     * @param dy 
+     * @param dx the first component of the translation vector on the screen
+     * @param dy the second component of the translation vector on the screen
      */
     public void translate(float dx, float dy);
 
     /**
-     * Initialize orbiting around a center point.
-     * @param orbitModifier must be a value between 0.0 and 1.0.
+     * Starts the orbiting function.
+     * 
+     * @param orbitModifier ignored by this method
      */
     public void startOrbit(float orbitModifier);
 
+    /**
+     * Updates the camera during orbiting by horizontal and vertical difference.
+     * 
+     * @param x horizontal difference
+     * @param y vertical difference
+     */
     public void updateOrbit(float x, float y);
 
+    /**
+     * Centers the box on the screen and updates the scale factor to see the
+     * entire box.
+     * 
+     * @param box the box
+     */
+    public void centerBox(AABB box);
+
+    /**
+     * Increases the scale factor by some amount.
+     * 
+     * @param by the increased amount
+     */
     public void zoom(float x, float y, float by);
 
     /**
-     * Returns the relative camera zoom.
+     * Gets the current scale factor.
+     * 
+     * @return the scale factor
      */
     public float getZoom();
 
     /**
-     * Sets the relative camera zoom.
-     * @param relativeZoom float from interval [0.0, 1.0]
+     * Sets the current scale factor.
+     * 
+     * @param relativeZoom the scale factor
      */
     public void setZoom(float relativeZoom);
-    
-    /**
-     * Centers the box on the screen.
-     * 
-     * @param box the box to center
-     */
-    public void centerBox(AABB box);
+            
 }
